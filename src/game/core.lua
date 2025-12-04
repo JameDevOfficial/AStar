3d898e52ac93b3d99e5456736347d08e81ea0d50
JameDevOfficial <jame.bayerl@gmail.com>
2025-12-04 20:10:24 +0100

    feat: init with imported files

---
+++ src/game/core.lua
@@ -0,0 +1,101 @@
+-- ⠀⠀⡀⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+-- ⠀⠘⢿⣝⠛⠋⠉⠉⠉⣉⠩⠍⠉⣿⠿⡭⠉⠛⠃⠲⣞⣉⡙⠿⣇⠀⠀⠀
+-- ⠀⠀⠈⠻⣷⣄⡠⢶⡟⢁⣀⢠⣴⡏⣀⡀⠀⠀⣠⡾⠋⢉⣈⣸⣿⡀⠀⠀
+-- ⠀⠀⠀⠀⠙⠋⣼⣿⡜⠃⠉⠀⡎⠉⠉⢺⢱⢢⣿⠃⠘⠈⠛⢹⣿⡇⠀⠀
+-- ⠀⠀⠀⢀⡞⣠⡟⠁⠀⠀⣀⡰⣀⠀⠀⡸⠀⠑⢵⡄⠀⠀⠀⠀⠉⠀⣧⡀
+-- ⠀⠀⠀⠌⣰⠃⠁⣠⣖⣡⣄⣀⣀⣈⣑⣔⠂⠀⠠⣿⡄⠀⠀⠀⠀⠠⣾⣷
+-- ⠀⠀⢸⢠⡇⠀⣰⣿⣿⡿⣡⡾⠿⣿⣿⣜⣇⠀⠀⠘⣿⠀⠀⠀⠀⢸⡀⢸
+-- ⠀⠀⡆⢸⡀⠀⣿⣿⡇⣾⡿⠁⠀⠀⣹⣿⢸⠀⠀⠀⣿⡆⠀⠀⠀⣸⣤⣼
+-- ⠀⠀⢳⢸⡧⢦⢿⣿⡏⣿⣿⣦⣀⣴⣻⡿⣱⠀⠀⠀⣻⠁⠀⠀⠀⢹⠛⢻
+-- ⠀⠀⠈⡄⢷⠘⠞⢿⠻⠶⠾⠿⣿⣿⣭⡾⠃⠀⠀⢀⡟⠀⠀⠀⠀⣹⠀⡆
+-- ⠀⠀⠀⠰⣘⢧⣀⠀⠙⠢⢤⠠⠤⠄⠊⠀⠀⠀⣠⠟⠀⠀⠀⠀⠀⢧⣿⠃
+-- ⠀⣀⣤⣿⣇⠻⣟⣄⡀⠀⠘⣤⣣⠀⠀⠀⣀⢼⠟⠀⠀⠀⠀⠀⠄⣿⠟⠀
+-- ⠿⠏⠭⠟⣤⣴⣬⣨⠙⠲⢦⣧⡤⣔⠲⠝⠚⣷⠀⠀⠀⢀⣴⣷⡠⠃⠀⠀
+-- ⠀⠀⠀⠀⠀⠉⠉⠉⠛⠻⢛⣿⣶⣶⡽⢤⡄⢛⢃⣒⢠⣿⣿⠟⠀⠀⠀⠀
+-- ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠁⠀⠁⠀⠀⠀⠀⠀
+-- "Here she comes! Keep testing! Remember: you never saw me!"
+
+local Core = {}
+Core.gameStarted = 0
+Core.finalTime = 0
+
+EXITED = -1
+PAUSED = 0
+LOADING = 1
+INHELP = 5
+INMENU = 11
+INGAME = 12
+
+Core.reset = function()
+
+end
+
+Core.load = function()
+    Core.status = LOADING
+    math.randomseed(os.time())
+    Core.screen = UI.windowResized()
+    if love.system.getOS() == "Android" or love.system.getOS() == "iOS" then
+        Core.isMobile = true
+    else
+        Core.isMobile = false
+    end
+
+    Core.status = INMENU
+end
+
+Core.update = function(dt)
+    if Core.status == INMENU then
+    elseif Core.status == INGAME then
+    end
+end
+
+Core.keypressed = function(key, scancode, isrepeat)
+    if key == "f5" then
+        Settings.DEBUG = not Settings.DEBUG
+    end
+    if Core.status == INHELP then
+        Core.status = INMENU
+        return
+    end
+    if Core.status == INMENU then
+        if key == "return" then
+            Core.reset()
+            Core.status = INGAME
+            Core.gameStarted = love.timer.getTime()
+        end
+        if key == "h" or key == "H" then
+            Core.status = INHELP
+        end
+    end
+
+    if Core.status == INGAME then
+        if key == "space" then
+
+        end
+    end
+end
+
+
+Core.mousepressed = function(x, y, button, istouch, presses)
+    if Core.status == INHELP and Core.isMobile then
+        Core.status = INMENU
+        return
+    end
+
+    if Core.status == INMENU and Core.isMobile then
+        Core.reset()
+        Core.status = INGAME
+        Core.gameStarted = love.timer.getTime()
+    end
+
+    if Core.status ~= INGAME or Core.revealedButtons >= 2 or button ~= 1 then
+        return
+    end
+end
+
+Core.mousemoved = function(x, y, dx, dy, istouch)
+    if Core.status == INGAME then
+    end
+end
+
+return Core
---
+++ src/game/settings.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.DEBUG = false
+
+M.buttons = {}
+M.buttons.delay = 2
+
+M.fonts = {}
+M.fonts.quirkyRobot = "assets/fonts/QuirkyRobot.ttf"
+M.fonts.semiCoder = "assets/fonts/SemiCoder.otf"
+M.fonts.courierPrimeCode = "assets/fonts/CourierPrimeCode/Courier Prime Code.ttf"
+
+return M;
